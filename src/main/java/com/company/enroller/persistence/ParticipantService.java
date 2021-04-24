be2a34e2a15f004c5f4d6455c1e479a3cd48d377
Oskar Kawecki <oskar.kawecki@schibsted.com>
2021-04-24 15:28:18 +0200

    First Commit

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,9 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import javax.websocket.Session;
+
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -9,14 +12,40 @@ import com.company.enroller.model.Participant;
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+	Session session;
 
 	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
+		return session.createCriteria(Participant.class).list();
 	}
+	
+	public Participant findByLogin(String login) {
+		Participant participant = (Participant) session.get(Participant.class, login);
+		return participant;
+
+	}
+
+	public void add(Participant participant) {
+		Transaction transaction = this.session.beginTransaction();
+		session.save(participant);
+	transaction.commit();
+
+}
+
+public void delete(Participant participant) {
+	Transaction transaction = this.session.beginTransaction();
+	session.delete(participant);
+	transaction.commit();
+
+}
+
+public void update(Participant participant) {
+	Transaction transaction = this.session.beginTransaction();
+	session.merge(participant);
+	transaction.commit();
+}
 
 }
